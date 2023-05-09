4a58d4f635d979a71df3908c48669f0d28d2e701
Pskdr <juanca.naranjo03@gmail.com>
2023-05-09 18:49:45 -0500

    fix: fixing build

--- src/App.js
+++ src/App.js
@@ -17,11 +17,6 @@ function App() {
   };
 
   // Generate string of checked items
-  const checkedItems = checked.length
-    ? checked.reduce((total, item) => {
-        return total + ", " + item;
-      })
-    : "";
 
   // Return classes based on whether item is checked
   var isChecked = (item) =>
